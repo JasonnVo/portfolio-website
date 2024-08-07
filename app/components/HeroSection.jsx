d8fd65903b0512507b1ffe1f8d87206064ef2343
Jason Vo <jason54423@gmail.com>
2024-08-07 02:34:10 -0400

    speed insights

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
 import DownloadButton from './DownloadButton';
 import LinkedinButton from './LinkedinButton';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 // Hero section 
 const HeroSection = () => {
   return (
     <section>
+      <SpeedInsights />
       <div className='grid grid-cols-1 lg:grid-cols-12'>
         <div className="col-span-7 place-self-center">
           <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold font-sans">
